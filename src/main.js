373261af26ad83b573bdd70a5ac921d9285c3d1a
William Chen <wiwechen@ucsc.edu>
2023-04-18 15:59:50 -0700

    Finished rocket, starting the Spaceship class

--- src/main.js
+++ src/main.js
@@ -9,3 +9,7 @@ let game = new Phaser.Game(config);
 // set UI sizes
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
+
+// reserve keyboard vars
+let keyF, keyR, keyLEFT, keyRIGHT;
+
