f7a1dcd3a177ee0102520959dd4342b222d5bbad
William Chen <wiwechen@ucsc.edu>
2023-04-18 15:27:11 -0700

    finish load

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -3,6 +3,12 @@ class Play extends Phaser.Scene{
         super("playScene");
     }
 
+    preload(){
+        this.load.image('rocket', './assets/rocket.png');
+        this.load.image('spaceship','./assets/spaceship.png');
+        this.load.image('starfield', './assets/starfield.png');
+    }
+
     create(){
         //
         //green UI background
