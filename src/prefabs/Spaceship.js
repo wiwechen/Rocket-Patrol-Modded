1d57eceff6759afa8ce27a3eaea76f6cae78194f
William Chen <wiwechen@ucsc.edu>
2023-04-18 16:11:59 -0700

    Spaceships done. Working on Shooting now

---
+++ src/prefabs/Spaceship.js
@@ -0,0 +1,17 @@
+//Spaceship prefab
+class Spaceship extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame, pointValue){
+        super(scene, x, y, texture, frame);
+        scene.add.existing(this);
+        this.points = pointValue;
+        this.moveSpeed = 3;
+    }
+
+    update(){
+        this.x -= this.moveSpeed;
+        //wrap around from left edge to right edge
+        if(this.x <= 0 - this.width){
+            this.x = game.config.width;
+        }
+    }
+}
