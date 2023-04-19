9f717ed208217b8c89697b7466d998412ea1a625
William Chen <wiwechen@ucsc.edu>
2023-04-19 15:25:22 -0700

    Functional Menu is Complete

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -64,11 +64,11 @@ class Play extends Phaser.Scene{
        //GAME OVER flag
        this.gameOver = false;
        
-       //60-second play clock
+       //Timer play clock
        scoreConfig.fixedWidth = 0;
-       this.clock = this.time.delayedCall(60000, () => {
+       this.clock = this.time.delayedCall(game.settings.Spaceship, () => {
         this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/2 +64, 'Press (R) to Restart', scoreConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 +64, 'Press (R) to Restart or <- for Menu', scoreConfig).setOrigin(0.5);
         this.gameOver = true;
        }, null, this);
     }
@@ -77,6 +77,10 @@ class Play extends Phaser.Scene{
         //check key input for restart
         if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
             this.scene.restart();
+        }
+
+        if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyLEFT)){
+            this.scene.start("menuScene");
         }
 
 
